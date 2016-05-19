2fad236b2214a90adac192c4a2cdba76a841e0e3
Abhimanyu V <abhimanyu.v@gmail.com>
2016-05-19 21:25:36 +0530

    nochanges - reformat code
    add pycharm idea files to gitignore

--- audio_tagger.py
+++ audio_tagger.py
@@ -9,128 +9,136 @@ import taglib
 from MediaInfoDLL import *
 
 # https://acoustid.org/webservice
-ACOUSTID_CLIENT_ID = "g4mgdCmxmd"	# registered id
+ACOUSTID_CLIENT_ID = "g4mgdCmxmd"  # registered id
 ACOUSTID_API_SERVER = "http://api.acoustid.org/v2/lookup"
 
 MI = MediaInfo()
 
+
 def find_file_extension(root, file_name):
-	ext = ""
-	codec = ""
-	container = ""
-	abs_file_path = os.path.join(root,file_name)
-	print "Analysing file ...", abs_file_path
-	MI.Open(abs_file_path)
-	container = MI.Get(Stream.General, 0, u"CodecID")
-	if not container:
-		codec = MI.Get(Stream.Audio, 0, u"Codec")
-		if codec in ["MPA2L3", "MPA1L3"]:
-			ext =".mp3"
-	else:
-		if container == "mp42":
-			ext = ".m4a"
-	print "container: {}, codec: {}".format(container, codec)
-	MI.Close()
-	return ext
+    ext = ""
+    codec = ""
+    container = ""
+    abs_file_path = os.path.join(root, file_name)
+    print "Analysing file ...", abs_file_path
+    MI.Open(abs_file_path)
+    container = MI.Get(Stream.General, 0, u"CodecID")
+    if not container:
+        codec = MI.Get(Stream.Audio, 0, u"Codec")
+        if codec in ["MPA2L3", "MPA1L3"]:
+            ext = ".mp3"
+    else:
+        if container == "mp42":
+            ext = ".m4a"
+    print "container: {}, codec: {}".format(container, codec)
+    MI.Close()
+    return ext
+
 
 def rename_and_append_ext(oldroot, newroot, name, ext):
-	filename, curr_ext = os.path.splitext(name)
-	if curr_ext != ext:
-		filename = filename + ext
-		old_file_path = os.path.join(oldroot, name)
-		new_file_path = os.path.join(newroot, filename)
-		print "rename {} to {}".format(old_file_path, new_file_path)
-		os.rename(old_file_path, new_file_path)
+    filename, curr_ext = os.path.splitext(name)
+    if curr_ext != ext:
+        filename = filename + ext
+        old_file_path = os.path.join(oldroot, name)
+        new_file_path = os.path.join(newroot, filename)
+        print "rename {} to {}".format(old_file_path, new_file_path)
+        os.rename(old_file_path, new_file_path)
+
 
 def rename_filename(oldroot, newroot, oldname, newname):
-	if not newname:
-		print "Empty new name, cant rename!"
-		return
+    if not newname:
+        print "Empty new name, cant rename!"
+        return
 
-	_, old_ext = os.path.splitext(oldname)
+    _, old_ext = os.path.splitext(oldname)
 
-	newname = newname + old_ext
-	old_file_path = os.path.join(oldroot, oldname)
-	new_file_path = os.path.join(newroot, newname)
-	print "rename {} to {}".format(old_file_path, new_file_path)
-	os.rename(old_file_path, new_file_path)
+    newname = newname + old_ext
+    old_file_path = os.path.join(oldroot, oldname)
+    new_file_path = os.path.join(newroot, newname)
+    print "rename {} to {}".format(old_file_path, new_file_path)
+    os.rename(old_file_path, new_file_path)
+
 
 def get_fingerprint(root, name):
-	FPCALC = "/usr/bin/fpcalc"
-	output = subprocess.check_output([FPCALC, os.path.join(root, name)])
-	output = output.split("\n")
-	duration = output[1].split("=")[1]
-	fingerprint = output[2].split("=")[1]
-	return fingerprint, duration
+    FPCALC = "/usr/bin/fpcalc"
+    output = subprocess.check_output([FPCALC, os.path.join(root, name)])
+    output = output.split("\n")
+    duration = output[1].split("=")[1]
+    fingerprint = output[2].split("=")[1]
+    return fingerprint, duration
+
 
 def get_audio_meta(name, fingerprint, duration):
-	# Add server url
-	url = ACOUSTID_API_SERVER
-	# Add client
-	url += "?client={}".format(ACOUSTID_CLIENT_ID)
-	# Add meta
-	url += "&meta=recordings+compress"
-	# Add duration
-	url += "&duration={}".format(duration)
-	# Add fingerprint
-	url += "&fingerprint={}".format(fingerprint)
+    # Add server url
+    url = ACOUSTID_API_SERVER
+    # Add client
+    url += "?client={}".format(ACOUSTID_CLIENT_ID)
+    # Add meta
+    url += "&meta=recordings+compress"
+    # Add duration
+    url += "&duration={}".format(duration)
+    # Add fingerprint
+    url += "&fingerprint={}".format(fingerprint)
 
-	print "Querying for metadata...", name
-	r = requests.get(url)
-	return r.text
+    print "Querying for metadata...", name
+    r = requests.get(url)
+    return r.text
+
 
 def parse_audio_json_data(audio_json_data):
-	title = ""
-	artist = ""
-	print "Json raw data", audio_json_data
-	data = json.loads(audio_json_data)
-	if data["status"] == "ok":
-		try:
-			# Only interested in first entry as that is with highest matching score
-			title = data["results"][0]["recordings"][0]["title"]
-			artist = data["results"][0]["recordings"][0]["artists"][0]["name"]
-		except (KeyError, IndexError):
-			print "Track metadata is not found in acoustid database, json_output: ", audio_json_data
-	else:
-		print "Status is NOK, json output: ", audio_json_data
-	return title.encode('utf-8'), artist.encode('utf-8')
+    title = ""
+    artist = ""
+    print "Json raw data", audio_json_data
+    data = json.loads(audio_json_data)
+    if data["status"] == "ok":
+        try:
+            # Only interested in first entry as that is with highest matching score
+            title = data["results"][0]["recordings"][0]["title"]
+            artist = data["results"][0]["recordings"][0]["artists"][0]["name"]
+        except (KeyError, IndexError):
+            print "Track metadata is not found in acoustid database, json_output: ", audio_json_data
+    else:
+        print "Status is NOK, json output: ", audio_json_data
+    return title.encode('utf-8'), artist.encode('utf-8')
+
 
 def add_tags_to_audio(root, name, title, artist):
-	f = taglib.File(os.path.join(root, name))
-	f.tags[u"ARTIST"] = [artist]
-	f.tags[u"TITLE"] = [title]
-	f.save()
-	print f.tags
+    f = taglib.File(os.path.join(root, name))
+    f.tags[u"ARTIST"] = [artist]
+    f.tags[u"TITLE"] = [title]
+    f.save()
+    print f.tags
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Audio auto tagger')
-	parser.add_argument("-p", "--path", type=str, required=True, help="Audio folder path")
-	parser.add_argument("-o", "--output-path", type=str, required=False, help="Output folder path")
+    parser = argparse.ArgumentParser(description='Audio auto tagger')
+    parser.add_argument("-p", "--path", type=str, required=True, help="Audio folder path")
+    parser.add_argument("-o", "--output-path", type=str, required=False, help="Output folder path")
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	in_path = os.path.abspath(os.path.expanduser(args.path))
-	print "Scanning ...", in_path
-	if args.output_path:
-		output_path =  os.path.abspath(os.path.expanduser(args.output_path))
-	else:
-		output_path = in_path
-	print "Output path ...", output_path
+    in_path = os.path.abspath(os.path.expanduser(args.path))
+    print "Scanning ...", in_path
+    if args.output_path:
+        output_path = os.path.abspath(os.path.expanduser(args.output_path))
+    else:
+        output_path = in_path
+    print "Output path ...", output_path
 
-	if not os.path.exists(output_path):
-		print "Creating output directory ...", output_path
-		os.makedirs(output_path)
+    if not os.path.exists(output_path):
+        print "Creating output directory ...", output_path
+        os.makedirs(output_path)
 
-	for root, dirs, files in os.walk(in_path, topdown=False):
-		for name in files:
-			ext = find_file_extension(root, name)
-			rename_and_append_ext (root, output_path, name, ext)
+    for root, dirs, files in os.walk(in_path, topdown=False):
+        for name in files:
+            ext = find_file_extension(root, name)
+            rename_and_append_ext(root, output_path, name, ext)
 
-	for root, dirs, files in os.walk(output_path, topdown=False):
-		for name in files:
-			fingerprint, duration = get_fingerprint(root, name)
-			audio_json_data = get_audio_meta(name, fingerprint, duration)
-			title, artist = parse_audio_json_data(audio_json_data)
-			print "Title={}, Artist={}".format(title, artist)
-			add_tags_to_audio(root, name, title, artist)
-			rename_filename(root, output_path, name, title)
+    for root, dirs, files in os.walk(output_path, topdown=False):
+        for name in files:
+            fingerprint, duration = get_fingerprint(root, name)
+            audio_json_data = get_audio_meta(name, fingerprint, duration)
+            title, artist = parse_audio_json_data(audio_json_data)
+            print "Title={}, Artist={}".format(title, artist)
+            add_tags_to_audio(root, name, title, artist)
+            rename_filename(root, output_path, name, title)
